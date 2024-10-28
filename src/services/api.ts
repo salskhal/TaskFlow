10d6baf5adb33aa882f1a566adf86991b3247c8a
salskhal <olaniyisal@gmail.com>
2024-10-28 07:39:02 +0100

    Feat/ test deployment

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,12 +2,14 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: 'http://localhost:5000/api',
+    // baseURL: 'http://localhost:5000/api',
+    baseURL: 'https://smart-task-manager-xye2.onrender.com/api',
     headers: {
         "Content-Type": "application/json", // Set default headers
     },
-    withCredentials: true
+    withCredentials: true,
 });
+
 
 
 export default api
